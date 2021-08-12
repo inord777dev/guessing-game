6b068c94310e4458e717aa287b6baefaeaf8a50b
inord777dev <inord777dev@gmail.com>
2021-08-12 09:11:15 +0300

    init: start and complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.min + Math.round((this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.max - Math.floor((this.max - this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.min + Math.round((this.max - this.min) / 2);
     }
 }
 
